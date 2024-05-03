aa60495d243e953afe9bd8c7c26d7b682f73a23e
Dario OCHOA <dario.martin.8a@gmail.com>
2024-05-03 18:09:27 -0300

    mejoras a calculadora de precios, lista de precios calculados, boton de eliminar, limpieza de form despues de cada calculo

--- proyectos_javascript/proyecto_js_04/script.js
+++ proyectos_javascript/proyecto_js_04/script.js
@@ -1,39 +1,96 @@
 console.log("hola a todos");
+//valores de entrada
 
-function calcular() {
-    //valores de entrada
-    const product1 = document.getElementById("product1").value;
-    console.log(product1);
-    const precio1 = parseFloat(document.getElementById("precio1").value);
-    const cantidad1 = parseFloat(document.getElementById("cantidad1").value);
+const resultados = document.getElementById("resultados");
+let unidad = "";
 
-    //unidades
-    var unidad1 = "";
-
-    //funcion poara obtener el radio seleccionado
+//funcion para obtener cual ruadio button de unidades esta seleccioando
+function unidades() {
+    //define que unidad usar
     if (document.getElementById("kg1").checked == true) {
         console.log("Kg");
-        unidad1 = "kg";
+        unidad = "kg";
     } else if (document.getElementById("g1").checked == true) {
         console.log("g");
-        unidad1 = "g";
+        unidad = "g";
     } else if (document.getElementById("l1").checked == true) {
         console.log("l");
-        unidad1 = "l";
+        unidad = "l";
+    } else if (document.getElementById("u1").checked == true) {
+        console.log("u");
+        unidad = "u";
+    } else {
+        alert("seleccione unidad");
     }
+}
 
-    //calculo
-    precioPorunidad1 = parseFloat(precio1 / cantidad1);
-    switch (unidad1) {
-        case "g":
-            var i = precioPorunidad1 * 1000;
-            document.getElementById("precioPorKg1").textContent = "el precio es $" + i + " por kg";
-            document.getElementById("precioPorg1").textContent = "el precio es $" + i / 1000 + " por g";
-            break;
-        case "kg":
-            var i = precioPorunidad1 * 1;
-            document.getElementById("precioPorKg1").textContent = "el precio es $" + i + " por kg";
-            document.getElementById("precioPorg1").textContent = "el precio es $" + i / 1000 + " por g";
-            break;
+function calcular() {
+    console.log("func calcular");
+    const precio = parseFloat(document.getElementById("precio").value);
+    const cantidad = parseFloat(document.getElementById("cantidad").value);
+    const product = document.getElementById("product").value;
+
+    if (product !== "" && !isNaN(cantidad) && !isNaN(precio)) {
+        //define que unidad usar
+        unidades();
+
+        //calculo
+        precioPorunidad = parseFloat(precio / cantidad);
+        console.log("precio" + precio);
+
+        console.log("precio por unidad" + precioPorunidad);
+        const p = document.createElement("p");
+        let i = 0;
+
+        switch (unidad) {
+            case "g":
+                i = (precioPorunidad * 1000).toFixed(2);
+                p.textContent = "el precio de " + product + " es $" + i + " por kg";
+                break;
+            case "kg":
+                i = (precioPorunidad * 1).toFixed(2);
+                p.textContent = "el precio de " + product + " es $" + i + " por kg";
+                console.log("kilogramo");
+                break;
+            case "l":
+                i = (precioPorunidad * 1).toFixed(2);
+                p.textContent = "el precio de " + product + " es $" + i + " por litro";
+                console.log("litro");
+                break;
+            case "u":
+                i = (precioPorunidad * 1).toFixed(2);
+                p.textContent = "el precio de " + product + " es $" + i + " por unidad";
+                console.log("unidad");
+                break;
+        }
+
+        console.log(p.textContent);
+        const li = document.createElement("li");
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn()); //llama a la funcion que le devuelve el boton para que agregue
+        resultados.appendChild(li);
+        console.log(p.textContent);
+
+        //limpiar form//
+        console.log("lsito el calculo");
+        document.getElementById("precio").value = "";
+        document.getElementById("product").value = "";
+        document.getElementById("cantidad").value = "";
     }
+}
+
+//devuvle el boton
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button"); //crea el delete button
+
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete"; //le da una clase para poder darle style
+
+    deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        resultados.removeChild(item);
+
+        const items = document.querySelectorAll("li");
+    });
+    return deleteBtn;
 }
